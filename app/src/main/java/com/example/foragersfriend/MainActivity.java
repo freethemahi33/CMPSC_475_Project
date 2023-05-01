8f11c8541c2d07a4fdc42a17d1c8a7fbc96874dd
Justin Schaffner <jschaffner500@gmail.com>
2023-04-30 21:33:54 -0400

    Created bottom nav bar

--- app/src/main/java/com/example/foragersfriend/MainActivity.java
+++ app/src/main/java/com/example/foragersfriend/MainActivity.java
@@ -1,28 +1,62 @@
 package com.example.foragersfriend;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.foragersfriend.NavFragments.HomeFragment;
+import com.example.foragersfriend.NavFragments.MushroomAddFragment;
+import com.example.foragersfriend.NavFragments.MushroomListFragment;
+import com.example.foragersfriend.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setTheme(R.style.Theme_ForagersFriend);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home_fragment:
+                    replaceFragment(new HomeFragment());
+                    Log.d(TAG, "onCreate: Home");
+                    break;
+                case R.id.mushroom_add_fragment:
+                    replaceFragment(new MushroomAddFragment());
+                    Log.d(TAG, "onCreate: Add");
+                    break;
+                case R.id.mushroom_list_fragment:
+                    replaceFragment(new MushroomListFragment());
+                    Log.d(TAG, "onCreate: List");
+                    break;
+            }
+            return true;
+        });
     }
 
-    public void onButtonClicked(View view) {
-        Intent intent = new Intent(this, MushroomList.class);
-        startActivity(intent);
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 
     @Override
